bb2f376ad803be0cc0bc627ad6472870c5039ccf
gabrie30 <jgabriels30@gmail.com>
2016-03-25 12:30:53 -0700

    add exception handling and update readme

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 #### Potential issues
 - Unsuccessful API calls will still save a response to the DB. This might be undesirable. To fix this we could check for a 200 status before saving the response. However, given the instructions for our method to return what FullContact.person would return, I chose to save whatever the response was.
+- Cache contents will eventually become stale
--- app/models/full_contact_lookup.rb
+++ app/models/full_contact_lookup.rb
@@ -4,15 +4,36 @@ class FullContactLookup < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   serialize :data
 
-  def self.by_email(email)
-    user = FullContactLookup.find_by(email: email)
+  def self.by_email(email=nil)
+      return ArgumentError.new("Please provide an email for lookup") if email.nil?
+      
+      user = FullContactLookup.find_by(email: email)
 
-    if !user
-      user_info = FullContact.person(email: email)
-      user = FullContactLookup.new(email: email, data: user_info)
-      user.save!
-    end
+      if !user
+        begin
+          user_info = FullContact.person(email: email)
+        rescue FullContact::Invalid => invalid_error 
+          return "Invalid request: #{invalid_error}"
+        rescue FullContact::NotFound => not_found_error
+          return "Email not found: #{not_found_error}"
+        end
 
-    user.data
+        if user_info.status == 200
+          user = FullContactLookup.new(email: email, data: user_info)
+          user.save!
+        else
+          return "#{user_info}"
+        end
+      end
+
+      user.data
   end
 end
+
+
+# handles no arguments being passed => ""
+# handles emails that do not exist  => "lalaalllala@kfjfjdkdkjf.com"
+# handles invalid emails            => 33234, "jfjfj"
+# handles valid emails              => "jgabriels30@gmail.com"
+
+# Only saves 202 responses
