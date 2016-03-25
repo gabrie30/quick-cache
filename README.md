382593f7437e488f237972bb3d278a01cc965599
gabrie30 <jgabriels30@gmail.com>
2016-03-24 20:37:35 -0700

    finish full_contact_lookup model logic and update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # quick-cache
+
+#### Potential issues
+- Unsuccessful API calls will still save a response to the DB. This might be undesirable. To fix this we could check for a 200 status before saving the response. However, given the instructions to return what the FullContact.person would return, I chose to save whatever the response was.
--- app/models/full_contact_lookup.rb
+++ app/models/full_contact_lookup.rb
@@ -1,2 +1,18 @@
 class FullContactLookup < ActiveRecord::Base
+  require 'fullcontact'
+
+  validates :email, presence: true, uniqueness: true
+  serialize :data
+
+  def self.by_email(email)
+    user = FullContactLookup.find_by(email: email)
+
+    if !user
+      user_info = FullContact.person(email: email)
+      user = FullContactLookup.new(email: email, data: user_info)
+      user.save!
+    end
+
+    user.data
+  end
 end
